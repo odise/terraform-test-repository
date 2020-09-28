20b6bb7860eeeea4d31cd384f515039644994825
Jan Nabbefeld <jan@odise.de>
2020-09-28 15:45:53 +0200

    bucket labels

--- stacks/gcp-stack/main.tf
+++ stacks/gcp-stack/main.tf
@@ -8,6 +8,12 @@ resource google_storage_bucket auto_expire {
   location      = "EU"
   force_destroy = true
 
+  labels = {
+    environment = var.environment
+    owner       = var.owner
+    project     = var.project
+  }
+  
   lifecycle_rule {
     condition {
       age = "3"
